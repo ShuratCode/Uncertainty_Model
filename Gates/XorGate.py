d3d20ac2728ca3bb3fd7d997808e829679ac5ba4
Shaked Eyal <shaked.eyal@gmail.com>
2019-04-08 21:58:32 +0300

    add xor gate

---
+++ Gates/XorGate.py
@@ -0,0 +1,38 @@
+from Gates.Gate import Gate
+
+
+class XorGate(Gate):
+
+    def __init__(self, inputs: dict):
+        self.__inputs = inputs
+        self.__calculate_output()
+
+    def __calculate_output(self):
+        values = list(self.__inputs.values())
+        value_1 = values[0]
+        value_2 = values[1]
+
+        if value_1 == 1 and value_2 == 1:
+            self.__output = 0
+
+        if value_1 == 1 and value_2 == 0:
+            self.__output = 1
+
+        if value_1 == 0 and value_2 == 1:
+            self.__output = 1
+
+        if value_1 == 0 and value_2 == 0:
+            self.__output = 0
+
+    def get_output(self):
+        return self.__output
+
+    def get_input(self, key: str):
+        return self.__inputs.get(key)
+
+    def get_inputs(self):
+        return self.__inputs
+
+    def set_input(self, key: str, value: int):
+        self.__inputs[key] = value
+        self.__calculate_output()
---
+++ Tests/Gates/test_xor_gate.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+
+from Gates.XorGate import XorGate
+
+
+class TestXorGate(TestCase):
+
+    def test_xor_two_1(self):
+        inputs = {'i1': 1, 'i2': 1}
+        xor = XorGate(inputs)
+        self.assertEqual(xor.get_output(), 0)
+
+    def test_xor_two_0(self):
+        inputs = {'i1': 0, 'i2': 0}
+        xor = XorGate(inputs)
+        self.assertEqual(xor.get_output(), 0)
+
+    def test_xor_1_and_0(self):
+        inputs = {'i1': 1, 'i2': 0}
+        xor = XorGate(inputs)
+        self.assertEqual(xor.get_output(), 1)
+
+    def test_set_input(self):
+        inputs = {'i1': 1, 'i2': 0}
+        xor = XorGate(inputs)
+        xor.set_input('i1', 0)
+        self.assertEqual(xor.get_output(), 0)
+
+    def test_get_input(self):
+        inputs = {'i1': 1, 'i2': 0}
+        xor = XorGate(inputs)
+        self.assertEqual(xor.get_input('i1'), 1)
